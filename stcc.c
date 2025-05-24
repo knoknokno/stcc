d425548ed59964810bdc0b698387c4b80b1bc39a
Konno Toshiki <s1811488@s.tsukuba.ac.jp>
2025-05-25 00:54:50 +0900

    構文木生成とスタックマシンの実装。四則演算の実装

--- stcc.c
+++ stcc.c
@@ -5,24 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 char* user_input;
-
-typedef enum {
-    TK_RESEVERD,
-    TK_NUM,
-    TK_EOF,
-} TokenKind;
-
-typedef struct Token Token;
-
-struct Token {
-    TokenKind kind;
-    Token* next;
-    int val;
-    char* str;
-};
-
-Token* token;
 
 void error(char* fmt, ...) {
     va_list ap;
@@ -44,6 +28,25 @@ void error_at(char* loc, char* fmt, ...) {
     fprintf(stderr, "\n");
     exit(1);
 }
+
+//==========Token==========
+
+typedef enum {
+    TK_RESEVERD,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token* next;
+    int val;
+    char* str;
+};
+
+Token* token;
 
 bool consume(char op) {
     if (token->kind != TK_RESEVERD || token->str[0] != op) {
@@ -91,7 +94,7 @@ Token* tokenize(char* p) {
             p++;
             continue;
         }
-        else if (*p == '+' || *p == '-') {
+        else if (strchr("+-*/()", *p)) {
             cur = new_token(TK_RESEVERD, cur, p++);
         }
         else if (isdigit(*p)) {
@@ -108,6 +111,118 @@ Token* tokenize(char* p) {
     return head.next;
 }
 
+//==========Node==========
+
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node* lhs;
+    Node* rhs;
+    int val;
+};
+
+Node* new_node(NodeKind kind, Node* lhs, Node* rhs) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node* new_node_num(int val) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node* expr();
+Node* mul();
+Node* primary();
+
+Node* expr() {
+    Node* node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        }
+        else {
+            return node;
+        }
+    }
+}
+
+Node* mul() {
+    Node* node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        }
+        else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        }
+        else {
+            return node;
+        }
+    }
+}
+
+Node* primary() {
+    if (consume('(')) {
+        Node* node = expr();
+        expect(')');
+        return node;
+    }
+    else {
+        return new_node_num(expect_number());
+    }
+}
+
+void gen(Node* node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません\n");
@@ -116,24 +231,17 @@ int main(int argc, char** argv) {
 
   user_input = argv[1];
   token = tokenize(user_input);
+  Node* node = expr();
 
+  // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-      if (consume('+')) {
-          printf("  add rax, %d\n", expect_number());
-      } else if (consume('-')) {
-          printf("  sub rax, %d\n", expect_number());
-      }
-      else {
-          return 1;
-      }
-  }
-
+  // スタックトップにある式全体の値をRAXにロードして返り値とする
+  printf("pop rax\n");
   printf("  ret\n");
   return 0;
 }
